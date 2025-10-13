1b462ddd54b69eb7dc2b227452d6c9843c9518ac
Teerath Ram <teerathjammar8@gmail.com>
2025-10-14 00:25:36 +0530

    complete toggleVideoLike controller

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -4,28 +4,43 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { Like } from "../models/like.models.js";
 
-const toggleVideoLike = asyncHandler( async(req, res) => {
-    const { videoId } = req.params;
-    
-}) 
+const toggleVideoLike = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  if (!videoId.trim() || !isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
 
-const toggleCommentLike = asyncHandler( async(req, res) => {
-    const { commentId } = req.params;
+  const like = await Like.findOne({
+    video: videoId,
+    user: req.user._id,
+  });
 
-}) 
+  if (!like) {
+    throw new ApiError(404, "Like not found");
+  }
 
-const toggleTweetLike = asyncHandler( async(req, res) => {
-    const { tweetId } = req.params;
+  const deletedLike = await Like.findOneAndDelete({
+    video: videoId,
+    user: req.user._id,
+  });
 
-}) 
+  if (!deletedLike) {
+    throw new ApiError(500, "Error while deleting like");
+  }
 
-const getLikedVideos = asyncHandler( async(req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, deletedLike, "Like deleted successfully"));
+});
 
-}) 
+const toggleCommentLike = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+});
 
-export {
-    toggleVideoLike,
-    toggleCommentLike,
-    toggleTweetLike,
-    getLikedVideos
-}
+const toggleTweetLike = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+});
+
+const getLikedVideos = asyncHandler(async (req, res) => {});
+
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos };
