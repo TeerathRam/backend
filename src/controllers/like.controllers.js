5822421c940c7b357aec123152590306b5c28401
Teerath Ram <teerathjammar8@gmail.com>
2025-10-14 00:32:36 +0530

    complete toggleTweetLike controller

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -64,6 +64,31 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
+  if (!tweetId.trim() || !isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet id");
+  }
+
+  const like = await Like.findOne({
+    tweet: tweetId,
+    user: req.user._id,
+  });
+
+  if (!like) {
+    throw new ApiError(404, "Like not found");
+  }
+
+  const deletedLike = await Like.findOneAndDelete({
+    tweet: tweetId,
+    user: req.user._id,
+  });
+
+  if (!deletedLike) {
+    throw new ApiError(500, "Error while deleting like");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, deletedLike, "Like deleted successfully"));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {});
