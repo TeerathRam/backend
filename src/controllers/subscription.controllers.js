b23522b8df0a3360ec3ab948eda4f57b7600442a
teerath <teerathjammar8@gmail.com>
2025-10-31 21:02:11 +0530

    complete toggleSubscription, getUserChannelSubscribers and getSubscribedChannels controller

--- src/controllers/subscription.controllers.js
+++ src/controllers/subscription.controllers.js
@@ -9,7 +9,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
 
   if (!channelId.trim() || !isValidObjectId(channelId)) {
-    throw new ApiError(400, "Invalid channel id");
+    throw new ApiError(400, "Invalid channel id.");
   }
 
   const subscription = await Subscription.findOne({
@@ -18,7 +18,7 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   });
 
   if (!subscription) {
-    throw new ApiError(400, "Channel not subscribed");
+    throw new ApiError(400, "Channel not subscribed.");
   }
 
   const deletedSubscription = await Subscription.findOneAndDelete({
@@ -27,20 +27,71 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   });
 
   if (!deletedSubscription) {
-    throw new ApiError(500, "Error while unsubscribing");
+    throw new ApiError(500, "Error while unsubscribing given channel.");
   }
 
   return res
     .status(200)
-    .json(new ApiResponse(200, {}, "Unsubscribed successfully"));
+    .json(new ApiResponse(200, {}, "Channel unsubscribed successfully."));
 });
 
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
   const { channelId } = req.params;
+  if (!channelId.trim() || !isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channel id.");
+  }
+
+  const channel = await User.findById(channelId);
+
+  if (!channel) {
+    throw new ApiError(400, "Channel not found.");
+  }
+
+  const subscribers = await Subscription.find({ channel: channelId }).populate(
+    "subscriber",
+    "username avatar coverImage"
+  );
+
+  const apiResponse = new ApiResponse(
+    200,
+    subscribers,
+    "User channel subscribers fetched successfully."
+  );
+
+  if (!apiResponse.data) {
+    apiResponse.message = "Channel has no subscribers.";
+  }
+
+  return res.status(200).json(apiResponse);
 });
 
 const getSubscribedChannels = asyncHandler(async (req, res) => {
   const { subscriberId } = req.params;
+  if (!subscriberId.trim() || !isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid channel id.");
+  }
+
+  const user = await User.findById(subscriberId);
+
+  if (!user) {
+    throw new ApiError(400, "User not found.");
+  }
+
+  const subscribedChannels = await Subscription.find({
+    subscriber: subscriberId,
+  }).populate("channel", "username avatar coverImage");
+
+  const apiResponse = new ApiResponse(
+    200,
+    subscribedChannels,
+    "Subscribed channels fetched successfully."
+  );
+
+  if (!apiResponse.data) {
+    apiResponse.message = "User has no subscribed channels.";
+  }
+
+  return res.status(200).json(apiResponse);
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
