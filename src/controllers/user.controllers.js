2daf523793b59c05ddb6ded3418aaa69dca45674
teerath <teerathjammar8@gmail.com>
2025-11-13 20:07:12 +0530

    fix: add period to error message for non-existent channel

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -454,7 +454,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   ]);
 
   if (!channel?.length) {
-    throw new ApiError(404, "Channel does not exist");
+    throw new ApiError(404, "Channel does not exist.");
   }
 
   return res
